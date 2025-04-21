4c296af43b3a5f53f1aef8b290b4ec6882d0cde5
Marty0793 <146634902+Marty0793@users.noreply.github.com>
2025-04-21 16:34:10 +0200

    Update app.py

--- gcalendar/backend/app.py
+++ gcalendar/backend/app.py
@@ -1,4 +1,3 @@
-print("✅ Flask backend started!")
 from flask import Flask, request, jsonify, send_from_directory
 from google_api import get_events, list_calendars, init_auth, exchange_code
 from config_handler import load_config
@@ -22,12 +21,8 @@ def events():
     config = load_config()
     return jsonify(get_events(config))
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
-    
 @app.route("/api/token", methods=["GET"])
 def token():
-    from backend.google_api import exchange_code
     code = request.args.get("code")
     if not code:
         return jsonify({"error": "missing code"}), 400
@@ -36,3 +31,11 @@ def token():
         return jsonify({"status": "authenticated"})
     else:
         return jsonify({"error": "failed to exchange code"}), 500
+
+if __name__ == "__main__":
+    print("✅ Flask HTTPS backend started!")
+    app.run(
+        host="0.0.0.0",
+        port=8000,
+        ssl_context=("/ssl/fullchain.pem", "/ssl/privkey.pem")
+    )
